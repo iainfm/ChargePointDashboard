bd92491baac42665ab9b3d29c5a4eecc5bea7b48
Iain McLaren <ifmclaren@gmail.com>
2022-01-03 16:54:39 +0000

    Python PoC added

---
+++ cps.py
@@ -0,0 +1,28 @@
+﻿import json
+import requests
+
+with open('cps.apikey', 'r') as file:
+    api_key = file.read().rstrip()
+
+url = 'https://account.chargeplacescotland.org/api/v2/poi/chargepoint/dynamic'
+
+headers = {'api-auth': api_key, 'chargePointIDs': '50690,50691,60238'}
+
+x = requests.get(url, headers=headers)
+y = json.loads(x.text)
+
+for item in y['chargePoints']:
+    for conn in item['chargePoint']['connectorGroups']:
+        for id in conn['connectors']:
+            print(item['chargePoint']['name'], id['connectorID'], id['connectorStatus'])
+
+# print (json.dumps(y['chargePoints']))
+
+
+# z=y['chargePoints'][0]['chargePoint']
+# z['name']
+
+# i = item
+# i['chargePoint']['connectorGroups'][0]['connectors'][0]['connectorStatus']
+
+# y['chargePoints'][0]['chargePoint']['connectorGroups'][1]['connectors'][0]['connectorStatus']
