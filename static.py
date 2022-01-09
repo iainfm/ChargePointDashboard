e0bbefbbffad43392bfed0924cd9ba92a9c1877d
Iain McLaren <ifmclaren@gmail.com>
2022-01-09 18:02:17 +0000

    Pull static data (cron script)

---
+++ static.py
@@ -0,0 +1,17 @@
+﻿#!/usr/bin/env python
+import requests
+
+with open('/cps.apikey', 'r') as file:
+    api_key = file.read().rstrip()
+
+url = 'https://account.chargeplacescotland.org/api/v3/poi/chargepoint/static'
+
+headers = {'api-auth': api_key}
+
+x = requests.get(url, headers=headers)
+body = x.content
+
+if (len(x.content) > 4096):
+	outf = open("/var/www/dbc/static.txt", "w")
+	outf.write(body)
+	outf.close
